6b251a19f12232003410f0935f78a8c161ec9ca6
lancelot <lancelot5050@gmail.com>
2018-01-08 17:46:41 +0900

    최종 수정버전

--- tsmain.cpp
+++ tsmain.cpp
@@ -1,8 +1,40 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<algorithm>
 using namespace std;
+
+bool byDate(const string& lhs, const string& rhs)
+{
+	string left{ lhs,2,10 };
+	string right{ rhs,2,10 };
+	return left < right;
+}
 
 int main()
 {
+	ifstream from("textfile1.txt");
 
+	if (!from.is_open())
+		return 0;
+
+	string line;
+	vector<string> inputArray;
+
+	while (!from.eof())
+	{
+		getline(from, line);
+		inputArray.push_back(line);
+	}
+
+	sort(inputArray.begin(), inputArray.end(), byDate);
+
+	ofstream to("output.txt");
+
+	for (unsigned int i = 0; i < inputArray.size(); ++i)
+		to << inputArray[i] << endl;
+
+	cin.get();
 	return 0;
 }
